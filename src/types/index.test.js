763c61b4d66312639621987298f16eebdaf30a18
xLink <xLink@cybershade.org>
2021-09-29 13:25:06 +0100

    fixed typings being set in gens properly

--- src/types/index.test.js
+++ src/types/index.test.js
@@ -1,6 +1,6 @@
 import * as gen_1 from './gen_1';
 import * as gen_2 from './gen_2';
-import * as gen_5 from './gen_5';
+import * as gen_6 from './gen_6';
 import { getTypesByGen } from './index';
 
 let expected = {
@@ -8,10 +8,10 @@ let expected = {
   2: 2,
   3: 2,
   4: 2,
-  5: 5,
-  6: 5,
-  7: 5,
-  8: 5,
+  5: 2,
+  6: 6,
+  7: 6,
+  8: 6,
 };
 
 Object.keys(expected).forEach(gen => {
