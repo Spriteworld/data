5176025534843bf37a7e38076c4e1737458fbd82
xLink <xLink@cybershade.org>
2021-09-30 17:10:25 +0100

    removed getPokedexByGen, bloody function XD

--- src/pokedex/index.js
+++ src/pokedex/index.js
@@ -1,29 +1,18 @@
 import * as gen_1 from './gen_1';
-// import * as gen_1_updated from './gen_1_updated';
+import * as gen_1_updated from './gen_1_updated';
 import * as gen_2 from './gen_2';
+import * as gen_2_updated from './gen_2_updated';
 import * as gen_3 from './gen_3';
+import * as gen_3_updated from './gen_3_updated';
 import * as gen_4 from './gen_4';
+import * as gen_4_updated from './gen_4_updated';
 import * as gen_5 from './gen_5';
+import * as gen_5_updated from './gen_5_updated';
 import * as gen_6 from './gen_6';
 import * as gen_7 from './gen_7';
+import * as gen_7_updated from './gen_7_updated';
 import * as gen_8 from './gen_8';
 import { GAMES } from '../index';
-
-export function getPokedexByGen(gen=8) {
-  switch(gen) {
-    case 1: return gen_1.POKEDEX; break;
-    case 2: return gen_2.POKEDEX; break;
-    case 3: return gen_3.POKEDEX; break;
-    case 4: return gen_4.POKEDEX; break;
-    case 5: return gen_5.POKEDEX; break;
-    case 6: return gen_6.POKEDEX; break;
-    case 7: return gen_7.POKEDEX; break;
-    case 8: return gen_8.POKEDEX; break;
-
-    default:
-      throw 'Gen "'+ gen +'" is invalid!';
-  }
-}
 
 export function getPokedexByGameId(gameId) {
   switch(gameId) {
@@ -50,37 +39,52 @@ export function getPokedexByGameId(gameId) {
 
     case GAMES.POKEMON_DIAMOND:
     case GAMES.POKEMON_PEARL:
+      return gen_4.POKEDEX;
+    break;
+
     case GAMES.POKEMON_PLATINUM:
+      return gen_4_updated.POKEDEX;
+    break;
+
     case GAMES.POKEMON_HEART_GOLD:
     case GAMES.POKEMON_SOUL_SILVER:
-      return gen_4.POKEDEX;
+      return gen_2_updated.POKEDEX;
     break;
 
     case GAMES.POKEMON_BLACK:
     case GAMES.POKEMON_WHITE:
+      return gen_5.POKEDEX;
+    break;
+
     case GAMES.POKEMON_BLACK_2:
     case GAMES.POKEMON_WHITE_2:
-      return gen_5.POKEDEX;
+      return gen_5_updated.POKEDEX;
     break;
 
     case GAMES.POKEMON_X:
     case GAMES.POKEMON_Y:
+      return gen_6.POKEDEX;
+    break;
+
     case GAMES.POKEMON_OMEGA_RUBY:
     case GAMES.POKEMON_ALPHA_SAPPHIRE:
-      return gen_6.POKEDEX;
+      return gen_3_updated.POKEDEX;
     break;
 
     case GAMES.POKEMON_SUN:
     case GAMES.POKEMON_MOON:
-    case GAMES.POKEMON_ULTRA_SUN:
-    case GAMES.POKEMON_ULTRA_MOON:
       return gen_7.POKEDEX;
     break;
 
-    // case GAMES.POKEMON_LETS_GO_PIKACHU:
-    // case GAMES.POKEMON_LETS_GO_EEVEE:
-    //   return gen_1_updated.POKEDEX;
-    // break;
+    case GAMES.POKEMON_ULTRA_SUN:
+    case GAMES.POKEMON_ULTRA_MOON:
+      return gen_7_updated.POKEDEX;
+    break;
+
+    case GAMES.POKEMON_LETS_GO_PIKACHU:
+    case GAMES.POKEMON_LETS_GO_EEVEE:
+      return gen_1_updated.POKEDEX;
+    break;
 
     case GAMES.POKEMON_SWORD:
     case GAMES.POKEMON_SHIELD:
--- src/pokedex/index.test.js
+++ src/pokedex/index.test.js
@@ -1,43 +1,19 @@
 import * as gen_1 from './gen_1';
+import * as gen_1_updated from './gen_1_updated';
 import * as gen_2 from './gen_2';
+import * as gen_2_updated from './gen_2_updated';
 import * as gen_3 from './gen_3';
+import * as gen_3_updated from './gen_3_updated';
 import * as gen_4 from './gen_4';
+import * as gen_4_updated from './gen_4_updated';
 import * as gen_5 from './gen_5';
+import * as gen_5_updated from './gen_5_updated';
 import * as gen_6 from './gen_6';
 import * as gen_7 from './gen_7';
+import * as gen_7_updated from './gen_7_updated';
 import * as gen_8 from './gen_8';
 import { GAMES } from '../index';
-import { getPokedexByGen, getPokedexByGameId } from './index';
-
-describe('getPokedexByGen works', () => {
-  test('can get gen_1 pokedex', () => {
-    expect(getPokedexByGen(1)).toBe(gen_1.POKEDEX);
-  });
-  test('can get gen_2 pokedex', () => {
-    expect(getPokedexByGen(2)).toBe(gen_2.POKEDEX);
-  });
-  test('can get gen_3 pokedex', () => {
-    expect(getPokedexByGen(3)).toBe(gen_3.POKEDEX);
-  });
-  test('can get gen_4 pokedex', () => {
-    expect(getPokedexByGen(4)).toBe(gen_4.POKEDEX);
-  });
-  test('can get gen_5 pokedex', () => {
-    expect(getPokedexByGen(5)).toBe(gen_5.POKEDEX);
-  });
-  test('can get gen_6 pokedex', () => {
-    expect(getPokedexByGen(6)).toBe(gen_6.POKEDEX);
-  });
-  test('can get gen_7 pokedex', () => {
-    expect(getPokedexByGen(7)).toBe(gen_7.POKEDEX);
-  });
-  test('can get gen_8 pokedex', () => {
-    expect(getPokedexByGen(8)).toBe(gen_8.POKEDEX);
-  });
-  test('gen 2000 doesnt exist yet!', () => {
-    expect(() => getPokedexByGen(2000)).toThrow('Gen "2000" is invalid!');
-  });
-});
+import { getPokedexByGameId } from './index';
 
 describe('getPokedexByGameId works', () => {
   let expected = {
@@ -55,23 +31,23 @@ describe('getPokedexByGameId works', () => {
     [GAMES.POKEMON_EMERALD]: gen_3.POKEDEX,
     [GAMES.POKEMON_DIAMOND]: gen_4.POKEDEX,
     [GAMES.POKEMON_PEARL]: gen_4.POKEDEX,
-    [GAMES.POKEMON_PLATINUM]: gen_4.POKEDEX,
-    [GAMES.POKEMON_HEART_GOLD]: gen_4.POKEDEX,
-    [GAMES.POKEMON_SOUL_SILVER]: gen_4.POKEDEX,
+    [GAMES.POKEMON_PLATINUM]: gen_4_updated.POKEDEX,
+    [GAMES.POKEMON_HEART_GOLD]: gen_2_updated.POKEDEX,
+    [GAMES.POKEMON_SOUL_SILVER]: gen_2_updated.POKEDEX,
     [GAMES.POKEMON_BLACK]: gen_5.POKEDEX,
     [GAMES.POKEMON_WHITE]: gen_5.POKEDEX,
-    [GAMES.POKEMON_BLACK_2]: gen_5.POKEDEX,
-    [GAMES.POKEMON_WHITE_2]: gen_5.POKEDEX,
+    [GAMES.POKEMON_BLACK_2]: gen_5_updated.POKEDEX,
+    [GAMES.POKEMON_WHITE_2]: gen_5_updated.POKEDEX,
     [GAMES.POKEMON_X]: gen_6.POKEDEX,
     [GAMES.POKEMON_Y]: gen_6.POKEDEX,
-    [GAMES.POKEMON_OMEGA_RUBY]: gen_6.POKEDEX,
-    [GAMES.POKEMON_ALPHA_SAPPHIRE]: gen_6.POKEDEX,
+    [GAMES.POKEMON_OMEGA_RUBY]: gen_3_updated.POKEDEX,
+    [GAMES.POKEMON_ALPHA_SAPPHIRE]: gen_3_updated.POKEDEX,
     [GAMES.POKEMON_SUN]: gen_7.POKEDEX,
     [GAMES.POKEMON_MOON]: gen_7.POKEDEX,
-    [GAMES.POKEMON_ULTRA_SUN]: gen_7.POKEDEX,
-    [GAMES.POKEMON_ULTRA_MOON]: gen_7.POKEDEX,
-    // [GAMES.POKEMON_LETS_GO_PIKACHU]: gen_1_updated.POKEDEX,
-    // [GAMES.POKEMON_LETS_GO_EEVEE]: gen_1_updated.POKEDEX,
+    [GAMES.POKEMON_ULTRA_SUN]: gen_7_updated.POKEDEX,
+    [GAMES.POKEMON_ULTRA_MOON]: gen_7_updated.POKEDEX,
+    [GAMES.POKEMON_LETS_GO_PIKACHU]: gen_1_updated.POKEDEX,
+    [GAMES.POKEMON_LETS_GO_EEVEE]: gen_1_updated.POKEDEX,
     [GAMES.POKEMON_SWORD]: gen_8.POKEDEX,
     [GAMES.POKEMON_SHIELD]: gen_8.POKEDEX,
   };
