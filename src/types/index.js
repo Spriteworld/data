a5c7ec9e8cfaa0f07ec8c5bdb4efd1b2e7ba6e28
xLink <xLink@cybershade.org>
2021-10-16 22:50:01 +0100

    added type effectiveness and tests

--- src/types/index.js
+++ src/types/index.js
@@ -6,6 +6,7 @@ import * as gen_2 from './gen_2';
 import * as gen_6 from './gen_6';
 // import * as gen_7 from './gen_6';
 // import * as gen_8 from './gen_6';
+import { inArray } from '../utilities';
 
 export function getTypesByGen(gen=8) {
   switch(gen) {
@@ -24,4 +25,53 @@ export function getTypesByGen(gen=8) {
       return gen_6.TYPES;
     break;
   }
+}
+
+/**
+ * Calculates type effectiveness given 2 types and the generational
+ * @param  string attkType
+ * @param  array defTypes
+ * @param  object types
+ * @return float representing effectiveness
+ *
+ * 0 = Ineffective
+ * 0.25
+ * 0.5 = Not very effective
+ * 1 = Normal
+ * 2
+ * 4 = Super Effective
+ */
+export function getTypeEffectiveness(attkType, defTypes, types) {
+  // do checks on the types
+
+  var effect = 1;
+  var noEffect = false;
+
+  defTypes.forEach(type => {
+    if (types.IMMUNITY[attkType].includes(type)) {
+      noEffect = true;
+      effect = 0;
+    } else if (types.RESIST[attkType].includes(type)) {
+      effect *= 0.5;
+    } else if (types.STRENGTH[attkType].includes(type)) {
+      if (effect <= 2) {
+        effect *= 2;
+      } else {
+        effect = 2;
+      }
+    }
+  });
+
+  // defneding pokemon is immune
+  if (noEffect) {
+    return 0;
+  }
+
+  // normal damage
+  if (effect === 0) {
+    return 1;
+  }
+
+  // otherwise return the multiplier
+  return effect;
 }
--- src/types/index.test.js
+++ src/types/index.test.js
@@ -1,7 +1,8 @@
 import * as gen_1 from './gen_1';
 import * as gen_2 from './gen_2';
 import * as gen_6 from './gen_6';
-import { getTypesByGen } from './index';
+import * as TYPES from './definitions';
+import { getTypesByGen, getTypeEffectiveness } from './index';
 
 describe('getTypesByGen can return types by generation number', () => {
   let expected = { 1: 1, 2: 2, 3: 2, 4: 2, 5: 2, 6: 6, 7: 6, 8: 6 };
@@ -79,5 +80,52 @@ describe('New Types get added across generations', () => {
   };
   test('types are correct for gen 6', () => {
     expect(gen_6.TYPES).toStrictEqual(expected_gen_6);
+  });
+});
+
+describe('getTypeEffectiveness against double typings', () => {
+  test('ground move against a steel/rock type is quad effective', () => {
+    expect(getTypeEffectiveness(TYPES.GROUND, [TYPES.STEEL, TYPES.ROCK], gen_6)).toBe(4);
+  });
+  test('flying move against a bug/grass type is quad effective', () => {
+    expect(getTypeEffectiveness(TYPES.FLYING, [TYPES.BUG, TYPES.GRASS], gen_6)).toBe(4);
+  });
+
+  test('ground move against a electric/water type is super effective', () => {
+    expect(getTypeEffectiveness(TYPES.GROUND, [TYPES.ELECTRIC, TYPES.WATER], gen_6)).toBe(2);
+  });
+
+  test('grass move against a water/flying type is normally effective', () => {
+    expect(getTypeEffectiveness(TYPES.GRASS, [TYPES.WATER, TYPES.FLYING], gen_6)).toBe(1);
+  });
+
+  test('bug move against a dragon/flying type is not very effective', () => {
+    expect(getTypeEffectiveness(TYPES.BUG, [TYPES.DRAGON, TYPES.FLYING], gen_6)).toBe(0.5);
+  });
+
+  test('fighting move against a psychic/flying type is not very effective', () => {
+    expect(getTypeEffectiveness(TYPES.FIGHTING, [TYPES.PSYCHIC, TYPES.FLYING], gen_6)).toBe(0.25);
+  });
+
+  test('electric move against a water/ground type is not effective', () => {
+    expect(getTypeEffectiveness(TYPES.ELECTRIC, [TYPES.WATER, TYPES.GROUND], gen_6)).toBe(0);
+  });
+});
+
+describe('getTypeEffectiveness against single typings', () => {
+  test('water move against a rock type is super effective', () => {
+    expect(getTypeEffectiveness(TYPES.WATER, [TYPES.ROCK], gen_6)).toBe(2);
+  });
+
+  test('normal move against a flying type is normally effective', () => {
+    expect(getTypeEffectiveness(TYPES.NORMAL, [TYPES.FLYING], gen_6)).toBe(1);
+  });
+
+  test('psychic move against a psychic type is not very effective', () => {
+    expect(getTypeEffectiveness(TYPES.PSYCHIC, [TYPES.PSYCHIC], gen_6)).toBe(0.5);
+  });
+
+  test('dragon move against a fairy type is not effective', () => {
+    expect(getTypeEffectiveness(TYPES.DRAGON, [TYPES.FAIRY], gen_6)).toBe(0);
   });
 });
