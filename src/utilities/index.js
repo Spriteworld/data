67f29a71b1b52dde3c786d6aad2749333050d1ef
xLink <xLink@cybershade.org>
2021-10-17 01:59:48 +0100

    added reusable utility functions

---
+++ src/utilities/index.js
@@ -0,0 +1,9 @@
+import validate from './validate';
+import round from './round';
+import randomInt from './randomInt';
+
+export {
+  validate,
+  round,
+  randomInt
+};
---
+++ src/utilities/randomInt.js
@@ -0,0 +1,3 @@
+export const randomInt = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
