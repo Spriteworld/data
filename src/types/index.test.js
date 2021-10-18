3c29a64b810d6602ff6f94f22e7e33b23c3d70b4
xLink <xLink@cybershade.org>
2021-10-18 16:43:08 +0100

    upgraded the single & dual type checking tests, ty PrudenGaming <3

--- src/types/index.test.js
+++ src/types/index.test.js
@@ -1,8 +1,7 @@
 import * as gen_1 from './gen_1';
 import * as gen_2 from './gen_2';
 import * as gen_6 from './gen_6';
-import * as TYPES from './definitions';
-import { getTypesByGen, getTypeEffectiveness } from './index';
+import { getTypesByGen } from './index';
 
 describe('getTypesByGen can return types by generation number', () => {
   let expected = { 1: 1, 2: 2, 3: 2, 4: 2, 5: 2, 6: 6, 7: 6, 8: 6 };
@@ -80,38 +79,5 @@ describe('New Types get added across generations', () => {
   };
   test('types are correct for gen 6', () => {
     expect(gen_6.TYPES).toStrictEqual(expected_gen_6);
-  });
-});
-
-describe('getTypeEffectiveness against double typings', () => {
-  let typeChecks = [
-    /** ATTACK_TYPE, DEFENSE_TYPES, GEN[1|2|6], expectation[0|0.25|0.5|1|2|4]  */
-    [TYPES.GROUND, [TYPES.STEEL, TYPES.ROCK], gen_6, 4],
-    [TYPES.FLYING, [TYPES.BUG, TYPES.GRASS], gen_6, 4],
-    [TYPES.GROUND, [TYPES.ELECTRIC, TYPES.WATER], gen_6, 2],
-    [TYPES.GRASS, [TYPES.WATER, TYPES.FLYING], gen_6, 1],
-    [TYPES.BUG, [TYPES.DRAGON, TYPES.FLYING], gen_6, 0.5],
-    [TYPES.FIGHTING, [TYPES.PSYCHIC, TYPES.FLYING], gen_6, 0.25],
-    [TYPES.ELECTRIC, [TYPES.WATER, TYPES.GROUND], gen_6, 0],
-  ];
-  typeChecks.forEach(check => {
-    test(`${check[0]} move against a ${check[1][0]}/${check[1][1]} type is ${check[3]}x effective`, () => {
-      expect(getTypeEffectiveness(check[0], check[1], check[2])).toBe(check[3]);
-    });
-  });
-});
-
-describe('getTypeEffectiveness against single typings', () => {
-  let typeChecks = [
-    /** ATTACK_TYPE, DEFENSE_TYPES, GEN[1|2|6], expectation[0|0.25|0.5|1|2|4]  */
-    [TYPES.WATER, [TYPES.ROCK], gen_6, 2],
-    [TYPES.NORMAL, [TYPES.FLYING], gen_6, 1],
-    [TYPES.PSYCHIC, [TYPES.PSYCHIC], gen_6, 0.5],
-    [TYPES.DRAGON, [TYPES.FAIRY], gen_6, 0],
-  ];
-  typeChecks.forEach(check => {
-    test(`${check[0]} move against a ${check[1][0]} type is ${check[3]}x effective`, () => {
-      expect(getTypeEffectiveness(check[0], check[1], check[2])).toBe(check[3]);
-    });
   });
 });
