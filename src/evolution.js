f77ab74527c2983b61f528d60a0bdfcd88ec7de3
xLink <xLink@cybershade.org>
2022-02-18 21:42:36 +0000

    added some more evo types

--- src/evolution.js
+++ src/evolution.js
@@ -7,12 +7,28 @@ export const EVOLUTION = {
   FRIENDSHIP_NIGHT: 'FRIENDSHIP_NIGHT',
   // Pokemon reaches the specified level
   LEVEL: 'LEVEL',
+  // levels up whilst knowing specific move
+  LEVEL_MOVE: 'MOVE',
   // Pokemon is traded
   TRADE: 'TRADE',
   // Pokemon is traded while it's holding the specified item
   TRADE_ITEM: 'TRADE_ITEM',
   // specified item is used on Pokemon
   ITEM: 'ITEM',
+  // specified item needs to be used on male Pokemon
+  ITEM_MALE: 'ITEM_MALE',
+  // specified item needs to be used on female Pokemon
+  ITEM_FEMALE: 'ITEM_FEMALE',
+  // specified item needs to be used on Pokemon at a specific time
+  ITEM_DAY: 'ITEM_DAY',
+  // specified item needs to be used on Pokemon at a specific time
+  ITEM_NIGHT: 'ITEM_NIGHT',
+  // levels up whilst in named area
+  LEVEL_AREA: 'AREA',
+  // evolves when level is met and male
+  LEVEL_MALE: 'LEVEL_MALE',
+  // evolves when level is met and female
+  LEVEL_FEMALE: 'LEVEL_FEMALE',
   // Pokemon reaches the specified level with attack > defense
   LEVEL_ATK_GT_DEF: 'LEVEL_ATK_GT_DEF',
   // Pokemon reaches the specified level with attack = defense
@@ -27,6 +43,8 @@ export const EVOLUTION = {
   LEVEL_NINJASK: 'LEVEL_NINJASK',
   // Pokemon reaches the specified level (special value for Shedinja)
   LEVEL_SHEDINJA: 'LEVEL_SHEDINJA',
+  // Pokemon reaches the specified level with a remoraid in the party
+  LEVEL_MANTINE: 'LEVEL_MANTINE',
   // Pokemon levels up with beauty ≥ specified value
   BEAUTY: 'BEAUTY',
 };
